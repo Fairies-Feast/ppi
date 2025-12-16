cf76b17cf262f411197b8055324b742d1b1a79db
Amsilla <86023563+Fairies-Feast@users.noreply.github.com>
2025-12-16 16:46:23 -0600

    Implement command handling in ppi-app.py

--- ppi-app.py
+++ ppi-app.py
@@ -1,3 +1,7 @@
-import os
+import sys
 
-print("Coming soon!")
+command = " ".join(sys.argv[1:])
+if command == "" or command == "help":
+  print("ppi help > Help\nppi run [file] > Run an exe, tar, etc file")
+elif command.startswith("run"):
+  print("Coming, soon!")

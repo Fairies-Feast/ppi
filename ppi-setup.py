6fe7d0e740d2fcb033bbb0a3dd18c16708574c19
Amsilla <86023563+Fairies-Feast@users.noreply.github.com>
2025-12-16 15:43:21 -0600

    Simplify PPI installation process
    Removed the progress loop during installation and added clear screen commands.

--- ppi-setup.py
+++ ppi-setup.py
@@ -5,12 +5,14 @@ import time
 
 def main():
     os.remove("install.sh")
-
-    for i in range(5):
-        print("PPI Setup has been installed!")
-        print("Please wait while we set up PPI.")
-        print(i*20 + "% done")
-        time.sleep(1)
+    os.system('clear')
+    print("Installing PPI...")
+    os.system("unalias ppi")
+    os.system('alias ppi="cd ~ && python3 ppi-app.py"')
+    os.system("curl https://ppi-source.pages.dev/ppi-app.py >> ppi-app.py")
+    os.system("clear")
+    print("PPI is installed!")
+    print("Use 'ppi' in the terminal to use PPI!")
 
 if __name__ == "__main__":
     main()

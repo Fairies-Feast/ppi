5fceb5d803a6a4dc5d4cf8b425664807b8f6c75b
Amsilla <86023563+Fairies-Feast@users.noreply.github.com>
2025-12-16 15:27:29 -0600

    Modify PPI setup process and messages
    Remove 'install.sh' and update setup messages.

--- ppi-setup.py
+++ ppi-setup.py
@@ -1,8 +1,17 @@
 import requests
+import os
+import time
 
 
 def main():
-    print("Try again later. We're still coding PPI.")
+    os.remove("install.sh")
+
+    for i in range(5):
+        print("PPI Setup has been installed!")
+        print("Please wait while we set up PPI.")
+        print(i*20 + "% done")
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
+ 
